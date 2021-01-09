ee593eea164470eb7f03fe2ee66033075e929af1
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 16:57:26 +0100

    träning om tostring och tolocalstring med join

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ friends = friends.toString();
 
 console.log(friends);
 */
-
+/*
 var friends = [
     "Hassan",
     "Soha",
@@ -27,3 +27,112 @@ console.log(friends); //This Is Array
 friends = friends.toString(); // Convert It To String
 
 console.log(friends); // String
+*/
+/*
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind"
+];
+
+console.log(friends); // This Is Array
+
+friends = friends.toLocaleString(); // locale string
+
+console.log(friends); // string
+*/
+
+/*
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind"
+];
+
+console.log(friends); // This Is Array
+
+friends = friends.toLocaleString(); // locale string
+
+console.log(friends); // string
+
+var myDate = new Date(),
+
+    myString = myDate.toString();
+
+console.log(myDate);
+
+console.log(myString);
+*/
+/*
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind"
+];
+
+console.log(friends); // This Is Array
+
+friends = friends.toLocaleString(); // locale string
+
+console.log(friends); // string
+
+var myDate = new Date(),
+
+    myString = myDate.toLocaleString();
+
+console.log(myDate);
+
+console.log(myString);
+*/
+/*
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind"
+];
+
+console.log(friends); 
+
+friends = friends.join();
+
+console.log(friends); 
+*/
+
+/*
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind"
+];
+
+console.log(friends); 
+
+friends = friends.join("-");
+
+console.log(friends); 
+*/
+
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind"
+];
+
+console.log(friends); 
+
+friends = friends.join("[]");
+
+console.log(friends); 
+
