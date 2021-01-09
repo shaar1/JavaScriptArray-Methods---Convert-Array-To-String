a18895ad76310b797948077cf5e205222d3f0d53
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 16:46:06 +0100

    träning om Array som är convert array to String

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 /* global console, alert, prompt*/
 
+/*
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind"
+];
 
+friends = friends.toString();
 
+console.log(friends);
+*/
+
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind"
+];
+
+console.log(friends); //This Is Array
+
+friends = friends.toString(); // Convert It To String
+
+console.log(friends); // String
